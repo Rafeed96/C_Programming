c57a6f461dff5edb86574553f31d360a1a0a815d
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-05-09 20:42:04 +0600

    Cpp file updated

--- Task_1.cpp
+++ Task_1.cpp
@@ -1,9 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
+int main()
+{
 
-    printf("Hello World!");
+    printf("Hello World!\n");
+    printf("This is my first program\tC if fun.\n");
+    printf("The question is - \"How to write a \\comment/ in C programming langauge?\"");
+
+
 
     return 0;
-
 }
