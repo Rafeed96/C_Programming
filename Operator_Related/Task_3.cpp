1ce32eaf0acd668b526b247151e0e887401a654e
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-05-09 23:56:07 +0600

    Update Task_3.cpp

--- Operator_Related/Task_3.cpp
+++ Operator_Related/Task_3.cpp
@@ -3,17 +3,25 @@
 int main()
 {
 
-    float a,b;
+    float a, b;
 
     /* X =  (3.31 * a2 + 2.01 * b3) / (7.16 * b2 + 2.01 * a3) */
     scanf("%f", &a);
     scanf("%f", &b);
 
+    float aS, aC, bS, bC;
+
+    aS = a * a;
+    aC = a * a * a;
+
+    bS = b * b;
+    bC = b * b * b;
+
     float X;
 
-    X = (3.31 * (a*a) + 2.01 * (b*b*b) / (7.16* (b*b)+ 2.01 * (a*a*a)));
+    X = ( 3.31*aS+2.01*bC) / (7.16*bS+ 2.01*aC);
 
-    printf("X = %f",X);
+    printf("X = %f", X);
 
     return 0;
 }
