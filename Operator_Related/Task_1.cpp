604864d999f89df853966502a8fab6367cb5293f
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-05-09 23:41:38 +0600

    Update Task_1.cpp

--- Operator_Related/Task_1.cpp
+++ Operator_Related/Task_1.cpp
@@ -6,5 +6,17 @@ int main()
     int a, b, res;
     float sum;
 
-    return 0;
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    res = a + b;
+    printf("Addition: %d\n", res);
+    res = a - b;
+    printf("Subtraction: %d\n", res);
+
+    printf("AMultiplication: %d\n", res);
+    res = a - b;
+    printf("Subtraction: %d\n", res);
+
+        return 0;
 }
