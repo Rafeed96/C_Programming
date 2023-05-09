08d647e52da23944d351e0f27ab21eeb3d16a52a
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-05-09 23:46:40 +0600

    Create Task_2.cpp

---
+++ Operator_Related/Task_2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+
+    float r;
+
+    scanf("%f", &r);
+
+    float area;
+    float pi = 3.141593;
+
+    pi = pi * 2;
+
+    area = pi * r;
+
+    printf("Area: %d\n", area);
+
+    return 0;
+}
