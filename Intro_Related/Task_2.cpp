076e08eb9507d22b894463b3c45bf4dc070fe61e
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-05-09 21:03:22 +0600

    Update Task_2.cpp

--- Intro_Related/Task_2.cpp
+++ Intro_Related/Task_2.cpp
@@ -11,6 +11,19 @@ int main()
     printf("My age is: %d\n",a);
     printf("My age is: %d\n",b);
 
+    int i;
+    float f;
+    char c;
+
+    
+    scanf("%d",&i);
+    scanf("%f",&f);
+    scanf("%c",&c);
+    
+    printf("\nThe interger value: %d\n", i);
+    printf("The floating point value: %f\n", f);
+    printf("The character value: %c\n", c);
+
 
     return 0;
 }
