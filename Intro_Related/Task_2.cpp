07d642497d6e580fda1d258b27283d5ade9fd817
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-05-09 21:18:33 +0600

    Update Task_2.cpp

--- Intro_Related/Task_2.cpp
+++ Intro_Related/Task_2.cpp
@@ -5,25 +5,21 @@ int main()
     int a;
     int b;
 
-    scanf("%d",&a);
-    scanf("%d",&b);
+    scanf("%d", &a);
+    scanf("%d", &b);
 
-    printf("My age is: %d\n",a);
-    printf("My age is: %d\n",b);
+    printf("My age is: %d\n", a);
+    printf("My age is: %d\n", b);
 
     int i;
     float f;
     char c;
 
-    
-    scanf("%d",&i);
-    scanf("%f",&f);
-    scanf("%c",&c);
-    
-    printf("\nThe interger value: %d\n", i);
+    scanf("%d %f %c", &i, &f, &c);
+
+    printf("\nThe integer value: %d\n", i);
     printf("The floating point value: %f\n", f);
     printf("The character value: %c\n", c);
-
 
     return 0;
 }
