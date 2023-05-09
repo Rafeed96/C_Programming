4632283a0180c26b748f514192710389ef3be5aa
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-05-09 20:45:27 +0600

    updated

--- Task_1.cpp
+++ Task_1.cpp
@@ -7,7 +7,13 @@ int main()
     printf("This is my first program\tC if fun.\n");
     printf("The question is - \"How to write a \\comment/ in C programming langauge?\"");
 
+    int i = 5;
+    float f = 3.141593;
+    char c = 'a';
 
+    printf("\nThe interger value: %d\n",i);
+    printf("The floating point value: %f\n",f);
+    printf("The character value: %c\n",c);
 
     return 0;
 }
